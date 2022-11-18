5bceb8fe4af8bc332518e164056502238de7ca2c
omocoool <edward.peter.cole@gmail.com>
2022-11-17 22:11:24 -0800

    make it work ez

--- main.lua
+++ main.lua
@@ -1,42 +1,48 @@
 return function()
-	local Settings = {
+	local s, e = pcall(function()
+	    local sg = Instance.new("ScreenGui",game:GetService('CoreGui'))
+	    local f = Instance.new("Frame",sg)
+	    f.Size = UDim2.new(0,5,0,5)
+	    local Settings = {
 		["mode"] = "fov",
 		-- part should be an R15 part or an R6 part inside a character
 		["part"] = "Head",
 		["character"] = "R6",
 		-- smoothness can be nil
-		["smoothness"] = 0.2,
-	}
-	local Players = game:GetService("Players")
-	local target
-	function toVector2(v3)
+		["smoothness"] = 5,
+	    }
+	    local Players = game:GetService("Players")
+	    local target
+	    function toVector2(v3)
 		return Vector2.new(v3.X,v3.Z)
-	end
-	function filter()
-		local LocalPlayer = Players.LocalPlayer
-		local Character = LocalPlayer.Character
-		if not Character then return end
-		local closest
-		for _, player in pairs(Players:GetPlayers()) do
-			if player == LocalPlayer then continue end
-			local char = player.Character	
-			if not char then continue end
-			local Head = char.Head
-			local Torso = char.Torso or char.UpperTorso
-			local dotProd = pos1:Dot(pos2)
-			local eye = camera.CFrame.Position
-		local fwd = camera.CFrame.LookVector
-		local p = Head
-		local prod = (p-(p-eye):Dot(fwd)*fwd).Magnitude
-			-- dot product is the angle between 2 vectors
-			if not closest or closest[1] > prod then
-				closest = {prod,char}
-				print(prod)
+	    end
+	    function filter()
+		--local s, e = pcall(function()
+			local LocalPlayer = Players.LocalPlayer
+			local Character = LocalPlayer.Character
+			if not Character then return end
+			local closest
+			local camera = workspace.CurrentCamera
+			for _, player in pairs(Players:GetPlayers()) do
+				if player == LocalPlayer then continue end
+				local char = player.Character	
+				if not char then continue end
+				local Head = char.Head
+				local eye = camera.CFrame.Position
+			local fwd = camera.CFrame.LookVector.Unit
+			local p = Head.Position
+
+			local prod = math.floor(math.acos((p-eye).Unit:Dot(fwd))*1000)/1000
+				-- dot product is the angle between 2 vectors
+				if not closest or closest[1] > prod then
+					closest = {prod,char}
+				end
 			end
-		end
-		return closest
-	end
-	function filterDistance()
+			return closest
+	       -- end)
+		--print(s,e)
+	    end
+	    function filterDistance()
 		local LocalPlayer = Players.LocalPlayer
 		local Character = LocalPlayer.Character
 		if not Character then return end
@@ -53,27 +59,40 @@ return function()
 				closest = {(pos1-pos2).Magnitude,char}
 			end
 		end
-	end
+	    end
 
-	function aim(target, smoothness)
+	    function aim(target, smoothness)
+		local s, e = pcall(function()
 		-- lower smoothness makes it smoother and therefore slower (but technically more accurate)
 		local camera = workspace.CurrentCamera
-		local targetx, targety = camera:WorldToViewportPoint(target.Position)
-		local targetPos = Vector2.new(targetx,targety)
-		local mousePos = game:GetService("UserInputService"):GetMouseLocation()
-			local input = game:GetService("VirtualInputManager")
-			-- virtualinputmanager can send virtual inputs in roblox (meaning that it can move your mouse, but only inside roblox)
-			local destination = mousePos:Lerp(targetPos,smoothness)
-			input:SendMouseMoveEvent(destination.X,destination.Y,workspace)
-	end
-	game:GetService("ContextActionService"):BindAction("jgejge4u89g893e4gu834gu89",function(input,state)
-		-- the cryptography thing is just to generate a random string and also to deter non synapse x users lol
-		-- the random string is sort of an anti-anticheat
-		if state == Enum.UserInputState.Begin then
-		if Settings.mode == "fov" then
-			target = filter()[2]
-			aim(target:FindFirstChild(Settings.part),Settings.smoothness)
+		local targetPosV3 = camera:WorldToViewportPoint(target.Position)
+
+		local targetPos = Vector2.new(targetPosV3.X,targetPosV3.Y)
+		f.Position = UDim2.new(0,targetPos.X,0,targetPos.Y)
+			local mousePos = game:GetService("UserInputService"):GetMouseLocation()
+
+		    local destination = (targetPos-mousePos)/smoothness
+		    print(mousePos.X,mousePos.Y)
+			mousemoverel(destination.X,destination.Y)
+		end)
+		print(s,e)
+	    end
+	    while wait() do
+		local function isMouseButtonDown()
+			for _,button in pairs(game:GetService("UserInputService"):GetMouseButtonsPressed()) do
+				if button.UserInputType == Enum.UserInputType.MouseButton2 then
+					return true
+				end
+			end
+			return false
+		end
+		if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.T) then
+		    local result = filter()
+		    if result then
+			aim(result[2]:FindFirstChild(Settings.part),Settings.smoothness or 5)
 		    end
 		end
-	end,false,Enum.UserInputType.MouseButton2)
+	    end
+	end)
+	print(s,e)
 end
